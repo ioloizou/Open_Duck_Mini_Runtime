86832e2747d3066705bf9a3b470bc15dff22c493
ioloizou <yiannisloizou@gmail.com>
2026-03-31 15:12:19 +0200

    Add velocity enviroment based inference script

---
+++ scripts/v2_velocity_rl_walk_mujoco.py
@@ -0,0 +1,351 @@
+import time
+import pickle
+
+import numpy as np
+from mini_bdx_runtime.rustypot_position_hwi import HWI
+from mini_bdx_runtime.onnx_infer import OnnxInfer
+
+from mini_bdx_runtime.raw_imu import Imu
+from mini_bdx_runtime.xbox_controller import XBoxController
+from mini_bdx_runtime.feet_contacts import FeetContacts
+from mini_bdx_runtime.eyes import Eyes
+from mini_bdx_runtime.sounds import Sounds
+from mini_bdx_runtime.antennas import Antennas
+from mini_bdx_runtime.projector import Projector
+from mini_bdx_runtime.rl_utils import make_action_dict, LowPassActionFilter
+from mini_bdx_runtime.duck_config import DuckConfig
+
+import os
+
+HOME_DIR = os.path.expanduser("~")
+
+
+class RLWalk:
+    def __init__(
+        self,
+        onnx_model_path: str,
+        duck_config_path: str = f"{HOME_DIR}/duck_config.json",
+        serial_port: str = "/dev/ttyACM0",
+        control_freq: float = 50,
+        pid=[30, 0, 0],
+        action_scale=0.25,
+        commands=False,
+        pitch_bias=0,
+        save_obs=False,
+        replay_obs=None,
+        cutoff_frequency=None,
+    ) -> None:
+
+        # Loading the imu orientation and the
+        self.duck_config = DuckConfig(config_json_path=duck_config_path)
+
+        self.commands = commands
+
+        self.pitch_bias = pitch_bias
+
+        self.onnx_model_path = onnx_model_path
+        self.policy = OnnxInfer(self.onnx_model_path, awd=True)
+
+        self.num_dofs = 14
+
+        # Found through motor indefication (rad/s)
+        self.max_motor_velocity = 5.24
+
+        # Control
+        self.control_freq = control_freq
+        self.pid = pid
+
+        self.save_obs = save_obs
+        if self.save_obs is not None:
+            self.saved_obs = []
+
+        self.replay_obs = replay_obs
+        if self.replay_obs is not None:
+            self.replay_obs = pickle.load(open(self.replay_obs, "rb"))
+
+        self.action_filter = None
+        if cutoff_frequency is not None:
+            self.action_filter = LowPassActionFilter(self.control_freq, cutoff_frequency)
+
+        self.hwi = HWI(self.duck_config, serial_port)
+
+        self.start()
+
+        self.imu = Imu(
+            sampling_freq=int(self.control_freq),
+            user_pitch_bias=self.pitch_bias,
+            upside_down=self.duck_config.imu_upside_down,
+        )
+
+        self.feet_contacts = FeetContacts()
+
+        # Scales
+        self.action_scale = action_scale
+
+        self.last_action = np.zeros(self.num_dofs)
+        self.last_last_action = np.zeros(self.num_dofs)
+        self.last_last_last_action = np.zeros(self.num_dofs)
+
+        self.init_pos = list(self.hwi.init_pos.values())
+
+        self.motor_targets = np.array(self.init_pos.copy())
+        self.prev_motor_targets = np.array(self.init_pos.copy())
+
+        self.last_commands = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+        self.paused = self.duck_config.start_paused
+
+        self.command_freq = 20
+        if self.commands:
+            self.xbox_controller = XBoxController(sampling_freq=self.command_freq)
+
+        # Optional expression features
+        if self.duck_config.eyes:
+            self.eyes = Eyes()
+        if self.duck_config.projector:
+            self.projector = Projector()
+        if self.duck_config.speaker:
+            self.sounds = Sounds(volume=1.0, sound_directory="../mini_bdx_runtime/assets")
+        if self.duck_config.antennas:
+            self.antennas = Antennas()
+
+    def get_obs(self):
+        imu_data = self.imu.get_imu_data()
+
+        dof_pos = self.hwi.get_present_positions(
+            ingore=[
+                "left_antenna",
+                "right_antenna",
+            ]
+        )  # rad
+
+        dof_vel = self.hwi.get_present_velocities(
+            ingore=[
+                "left_antenna",
+                "right_antenna",
+            ]
+        )  # rad/s
+
+        if dof_pos is None or dof_vel is None:
+            return None
+
+        if len(dof_pos) != self.num_dofs:
+            print(f"Expected {self.num_dofs} dof positions, but got {len(dof_pos)}")
+            return None
+
+        if len(dof_vel) != self.num_dofs:
+            print(f"Expected {self.num_dofs} dof velocities, but got {len(dof_vel)}")
+            return None
+
+        cmds = self.last_commands
+
+        projected_gravity = None
+        if "quarternion" in imu_data and imu_data["quaternion"] is not None:
+            imu_quat = imu_data["quaternion"]
+            if len(imu_quat) == 4 and all(q is not None for q in imu_quat):
+                try:
+                    projected_gravity = self.compute_projected_gravity(imu_data)
+                except Exception as e:
+                    print(f"Error computing projected gravity: {e}")
+
+        if projected_gravity is None:
+            # If we can't compute the projected gravity, we can use a default value
+            projected_gravity = np.array([0.0, 0.0, -1.0])
+
+        obs = np.concatenate(
+            [
+                imu_data["gyro"],
+                imu_data["accelero"],
+                projected_gravity,
+                dof_pos - self.init_pos,
+                dof_vel * 0.05,
+                self.last_action,
+                cmds,
+            ]
+        )
+
+    def quat_apply_inverse(self, quat, vec):
+        """Apply an inverse quaternion rotation to a vector."""
+        w = quat[0]
+        xyz = quat[1:4]
+
+        t = np.cross(xyz, vec)
+        return vec - w * t + np.cross(xyz, t)
+
+    def compute_projected_gravity(self, imu_data):
+        gravity = np.array([0, 0, -1])
+        projected_gravity = self.quat_apply_inverse(imu_data["orientation"], gravity)
+        return projected_gravity
+
+    def start(self):
+        kps = [self.pid[0]] * 14
+        kds = [self.pid[2]] * 14
+
+        # Lower head kps
+        kps[5:9] = [8] * 4
+
+        self.hwi.set_kps(kps)
+        self.hwi.set_kds(kds)
+        self.hwi.turn_on()
+
+        time.sleep(2)
+
+    def run(self):
+        i = 0
+        try:
+            print("Starting")
+            start_t = time.time()
+            while True:
+                left_trigger = 0
+                right_trigger = 0
+                t = time.time()
+
+                if self.commands:
+                    self.last_commands, self.buttons, left_trigger, right_trigger = (
+                        self.xbox_controller.get_last_command()
+                    )
+                    if self.buttons.X.triggered:
+                        if self.duck_config.projector:
+                            self.projector.switch()
+
+                    if self.buttons.B.triggered:
+                        if self.duck_config.speaker:
+                            self.sounds.play_random_sound()
+
+                    if self.duck_config.antennas:
+                        self.antennas.set_position_left(right_trigger)
+                        self.antennas.set_position_right(left_trigger)
+
+                    if self.buttons.A.triggered:
+                        self.paused = not self.paused
+                        if self.paused:
+                            print("Paused")
+                        else:
+                            print("Unpaused")
+
+                if self.paused:
+                    time.sleep(0.1)
+                    continue
+
+                obs = self.get_obs()
+                if obs is None:
+                    continue
+
+                if self.save_obs:
+                    self.saved_obs.append(obs)
+
+                if self.replay_obs is not None:
+                    if i >= len(self.replay_obs):
+                        print("Finished replaying obs")
+                        break
+                    obs = self.replay_obs[i]
+                    i += 1
+
+                action = self.policy.infer(obs)
+
+                self.last_action = action.copy()
+
+                self.motor_targets = self.init_pos + action * self.action_scale
+
+                if self.action_filter is not None:
+                    self.action_filter.push(self.motor_targets)
+                    filtered_motor_targets = self.action_filter.get_filtered_action()
+                    if time.time() - start_t > 1:
+                        self.motor_targets = filtered_motor_targets
+
+                self.prev_motor_targets = self.motor_targets.copy()
+
+                # To combine teleoperated head with RL policy
+                head_motor_targets = self.last_commands[3:] + self.motor_targets[5:9]
+                self.motor_targets[5:9] = head_motor_targets
+
+                action_dict = make_action_dict(self.motor_targets, list(self.hwi.joints.keys()))
+
+                self.hwi.set_position_all(action_dict)
+
+                i += 1
+
+                took = time.time() - t
+                if (1 / self.control_freq - took) < 0:
+                    print(
+                        "Policy control budget exceeded by",
+                        np.around(took - 1 / self.control_freq, 3),
+                    )
+                time.sleep(max(0, 1 / self.control_freq - took))
+
+        except KeyboardInterrupt:
+            if self.duck_config.antennas:
+                self.antennas.stop()
+            if self.duck_config.eyes:
+                self.eyes.stop()
+            if self.duck_config.projector:
+                self.projector.stop()
+
+        if self.save_obs:
+            pickle.dump(self.saved_obs, open("robot_saved_obs.pkl", "wb"))
+        print("TURNING OFF")
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--onnx_model_path",
+        type=str,
+        required=True,
+        help="Path to the ONNX model file.",
+    )
+    parser.add_argument(
+        "--duck_config_path",
+        type=str,
+        default=f"{HOME_DIR}/duck_config.json",
+        help="Path to the duck config json file.",
+    )
+    
+    parser.add_argument("-a", "--action_scale", type=float, default=0.25)
+    parser.add_argument("-p", type=int, default=30)
+    parser.add_argument("-i", type=int, default=0)
+    parser.add_argument("-d", type=int, default=0)
+    parser.add_argument("-c", "--control_freq", type=int, default=50)
+    parser.add_argument("--pitch_bias", type=float, default=0, help="deg")
+    parser.add_argument(
+        "--commands",
+        action="store_true",
+        default=True,
+        help="external commands, keyboard or gamepad. Launch control_server.py on host computer",
+    )
+    parser.add_argument(
+        "--save_obs",
+        type=str,
+        required=False,
+        default=False,
+        help="save the run's observations",
+    )
+    parser.add_argument(
+        "--replay_obs",
+        type=str,
+        required=False,
+        default=None,
+        help="replay the observations from a previous run (can be from the robot or from mujoco)",
+    )
+    parser.add_argument("--cutoff_frequency", type=float, default=None)
+
+    args = parser.parse_args()
+    pid = [args.p, args.i, args.d]
+
+    print("Done parsing arguments, starting RL walk")
+    rl_walk = RLWalk(
+        args.onnx_model_path,
+        duck_config_path=args.duck_config_path,
+        action_scale=args.action_scale,
+        pid=pid,
+        control_freq=args.control_freq,
+        commands=args.commands,
+        pitch_bias=args.pitch_bias,
+        save_obs=args.save_obs,
+        replay_obs=args.replay_obs,
+        cutoff_frequency=args.cutoff_frequency,
+    )
+    print("Done instantiating RLWalk")
+    rl_walk.run()
